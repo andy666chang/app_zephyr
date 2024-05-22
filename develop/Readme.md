6d3a71502a7b2432d3cfcbe85a5d4256c28c8646
andy666chang <andy850821@gmail.com>
2024-05-22 23:48:44 +0800

    feat(m03x): add soc

---
+++ develop/Readme.md
@@ -0,0 +1,5 @@
+
+Build:
+```
+west build -p -b nrf52 develop\app\test -DZEPHYR_EXTRA_MODULES="D:\Andy\Desktop\app_zephyr\develop"
+```
---
+++ develop/soc/arm/nuvoton_m03x/m031/soc.h
@@ -0,0 +1,8 @@
+
+#ifndef ZEPHYR_SOC_NUVOTON_M031_SOC_H_
+#define ZEPHYR_SOC_NUVOTON_M031_SOC_H_
+
+#include <zephyr/sys/util.h>
+#include <NuMicro.h>
+
+#endif /* ZEPHYR_SOC_NUVOTON_M03X_SOC_H_*/
