64bb3e0bbb968055c2267c4673e31e0353dc4f1f
andy666chang <andy850821@gmail.com>
2025-03-09 00:50:52 +0800

    feat(led): apply led blink

--- develop/app/road_led/src/main.c
+++ develop/app/road_led/src/main.c
@@ -12,6 +12,7 @@
 
 int main(void)
 {
+	#if 0
 	const struct device *const dev = DEVICE_DT_GET_ONE(st_vl53l0x);
 	struct sensor_value value;
 	int ret;
@@ -39,4 +40,44 @@ int main(void)
 		k_sleep(K_MSEC(1000));
 	}
 	return 0;
+	#endif
 }
+
+
+#include <zephyr/drivers/gpio.h>
+
+/* size of stack area used by each thread */
+#define STACKSIZE 1024
+
+/* scheduling priority used by each thread */
+#define PRIORITY 7
+
+static void blink(void)
+{
+	const struct gpio_dt_spec spec = GPIO_DT_SPEC_GET(DT_NODELABEL(bd_led), gpios);
+	int ret;
+
+	printk("Thread: %s\n", __func__);
+
+	if (!device_is_ready(spec.port)) {
+		printk("Error: %s device is not ready\n", spec.port->name);
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&spec, GPIO_OUTPUT_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure pin %d\n",
+			ret, spec.pin);
+		return;
+	}
+
+	while (1) {
+		gpio_pin_set(spec.port, spec.pin, 1);
+		k_msleep(500);
+		gpio_pin_set(spec.port, spec.pin, 0);
+		k_msleep(500);
+	}
+}
+
+K_THREAD_DEFINE(blink_id, STACKSIZE, blink, NULL, NULL, NULL,
+	PRIORITY, 0, 0);
